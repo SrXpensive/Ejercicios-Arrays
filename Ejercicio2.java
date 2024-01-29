e531d34bf1437807cd8a44b136a3ca4f98133e69
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-01-29 07:57:09 +0100

    Ejercicio 2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,5 +1,32 @@
 package org.example;
 
-public class Ejercicio2 {
+import leer.Leer;
 
+import java.util.Arrays;
+
+public class Ejercicio2 {
+    public static void ejercicio2(){
+        int [] numeros = new int [10];
+        int valor;
+        int pos;
+        String opcion;
+        do{
+            System.out.println("a. Mostrar valores");
+            System.out.println("b. Introducir valores ");
+            System.out.println("c. Salir");
+            opcion = Leer.leerTexto("Introduce una opción (a,b,o c): ");
+            switch(opcion){
+                case "a":
+                    System.out.println(Arrays.toString(numeros));
+                    break;
+                case "b":
+                    valor = Leer.leerEntero("Introduce un valor: ");
+                    pos = Leer.leerEntero("Ahora introduce una posición para ese valor: ");
+                    numeros[pos-1] = valor;
+                    break;
+            }
+            System.out.println("--------------------");
+
+        }while(!opcion.equals("c"));
+    }
 }
--- Main.java
+++ Main.java
@@ -2,10 +2,14 @@ package org.example;
 import leer.*;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1. Ejercicio 1");
+        System.out.println("2. Ejercicio 2");
         int opcion = Leer.leerEntero("Introduce una opción: ");
         switch(opcion){
             case 1:
                 Ejercicio1.ejercicio1();
+            case 2:
+                Ejercicio2.ejercicio2();
         }
     }
 }
